c6d5516f4beec011f496de75dedd725d3be49cb4
ESRAILHAQUE <esrailbblhs@gmail.com>
2024-02-19 15:59:18 +0600

    seat option done

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
 let purchaseSeats = [];
 let purchaseTicket = document.getElementById("purchaseTicket");
-let purchaseTicketNumber = parseInt(purchaseTicket.textContent);
 let totalAvailablesetsText = document.getElementById("totalAvailablesets");
-let availableSeats = parseInt(totalAvailablesetsText.textContent);
 let seatList = document.getElementById("seatList");
 let seatType = document.getElementById("seatType");
 let seatPrice = document.getElementById("seatPrice");
 
-
-
-
 function seatBooking(seat) {
-  purchaseSeats.push(seat.textContent);
+    let seatTextContent = seat.textContent.trim();
+    
+    if (purchaseSeats.length < 4) {
+          if (!purchaseSeats.includes(seatTextContent)) {
+            purchaseSeats.push(seatTextContent);
+            seat.classList.add("bg-primary-color","hover:bg-primary-color");
+              console.log(purchaseSeats);
+              let li = document.createElement("li");
+              li.textContent = seat.textContent.trim();
+              seatList.appendChild(li);
+              let liSeatType = document.createElement("li");
+              liSeatType.textContent = "Economics";
+              seatType.appendChild(liSeatType);
+              let liseatPrice = document.createElement("li");
+              liseatPrice.textContent = "550";
+              seatPrice.appendChild(liseatPrice);
+        }
+        
+        purchaseTicket.textContent = purchaseSeats.length;
+        totalAvailablesetsText.textContent = 40 - purchaseSeats.length;
+        
+    } else if (purchaseSeats.includes(seatTextContent)) {
+        alert("Seat is selected");
+    }
+    else {
+        alert("You can not purchase more than 4 tickets.");
+    }
+  
 
-  if (purchaseSeats.length <= 4 && availableSeats > 0) {
-    seat.classList.add("bg-primary-color", "hover:bg-primary-color");
-    availableSeats--;
-    purchaseTicketNumber++;
-    totalAvailablesetsText.textContent = availableSeats;
-      purchaseTicket.textContent = purchaseTicketNumber;
-      let li = document.createElement('li');
-      let seatListText = li.textContent(seat.textContent);
-      seatList.appendChild(seatListText);
-  } else {
-    alert(
-      "You can not purchase more than 4 tickets."
-    );
-  }
-}
+    
 
-function getInputById(inputId) {
-  inp;
 }
